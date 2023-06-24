b3479b9ac16b36de6b7e47179f11faa5e6abfa71
rpatel7-matc <137583398+rpatel7-matc@users.noreply.github.com>
2023-06-24 00:04:10 -0500

    course_project_milestone_1

---
+++ apache_log_analyzer.py
@@ -0,0 +1,11 @@
+#Ricky Patel
+#rpatel7@madisoncollege.edu
+#Apache log analyzer project milestone: one create var and print it, take an input and store it.
+
+#added output and printed
+output = "Apache log analyzer project"
+print(output,"\n")
+
+#input taken and printed
+input = input("y for yes n or no\n")
+print("we have recived the following input: ", input)
