e21e0fffa07c845ba983b6cfe198b13743baf07e
Ricky <rpatel@madisoncollege.edu>
2023-08-04 22:32:38 -0500

    Milestone 10

--- apache_log_analyzer.py
+++ apache_log_analyzer.py
@@ -4,7 +4,13 @@
 # rpatel7@madisoncollege.edu
 # Apache log analyzer project MILESTONE 1; create var and print it, take an input and store it.
 
-import getopt, sys, subprocess, argparse
+import getopt, sys, subprocess, argparse, requests, bs4
+
+#fuction calls website for info on most requested IP
+def IPLookUp(IP):
+    url = f"https://tools.keycdn.com/geo?host={IP}"
+    response = requests.get(url)
+    return response.text
 
 #processes an Apache log file using shell commands, counts the occurrences of each IP address, and returns 5 IP addresses with the highest occurrence counts.
 def IPAddressCount(apache_log_file_name):
@@ -25,22 +31,35 @@ def ParseLogEntry(apache_log_entry):
 def main():
 
     #create argumnet for input for file name
-    parser = argparse.ArgumentParser (description = "Parser")
+    #parser = argparse.ArgumentParser(description = "Parser")
 
     #argument for file name
-    parser.add_argument("-f", "--filenmae", dest = "filename", required = True, type = str, help = "Enter File Name")
+    #parser.add_argument("-f", "--filenmae", dest = "filename", required = True, type = str, help = "Enter File Name")
 
     #getting arguments user entered
-    args = parser.parse_args()
+    #args = parser.parse_args()
 
-    results = IPAddressCount(args.filename)
+    #results = IPAddressCount(args.filename)
+    results = IPAddressCount("m5-access.log")
+
+    #get just the IP of the IP that is seen the most
+    highest_ip = results.split("\n")[-2].split()[1]
+
+    print(highest_ip)
+
+    highest_url = IPLookUp(highest_ip)
+    print(highest_url[:250])
+
+    myHTML = bs4. BeautifulSoup(highest_url, features="html.parser")
+    print(myHTML.find_all("dd",class_ = "col-8 text-monospace")[1].text)
 
     apache_log_analysis = open("apache_analysis.txt", "w")
 
-    apache_log_analysis.write(results)
-    print(results)
+    apache_log_analysis.write(highest_url)
 
     apache_log_analysis.close
+
+    
             
 if __name__ == "__main__":
     main()
