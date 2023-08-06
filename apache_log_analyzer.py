5f3e9bbd418307b987b539a3f1654245345dc12f
Ricky <rpatel@madisoncollege.edu>
2023-08-05 19:21:07 -0500

    Milestone 11

--- apache_log_analyzer.py
+++ apache_log_analyzer.py
@@ -4,11 +4,11 @@
 # rpatel7@madisoncollege.edu
 # Apache log analyzer project MILESTONE 1; create var and print it, take an input and store it.
 
-import getopt, sys, subprocess, argparse, requests, bs4
+import getopt, sys, subprocess, argparse, requests, bs4, json
 
 #fuction calls website for info on most requested IP
 def IPLookUp(IP):
-    url = f"https://tools.keycdn.com/geo?host={IP}"
+    url = f"https://ipinfo.io/{IP}/json"
     response = requests.get(url)
     return response.text
 
@@ -31,35 +31,34 @@ def ParseLogEntry(apache_log_entry):
 def main():
 
     #create argumnet for input for file name
-    parser = argparse.ArgumentParser(description = "Parser")
+    #parser = argparse.ArgumentParser(description = "Parser")
 
     #argument for file name
-    parser.add_argument("-f", "--filenmae", dest = "filename", required = True, type = str, help = "Enter File Name")
+    #parser.add_argument("-f", "--filenmae", dest = "filename", required = True, type = str, help = "Enter File Name")
 
     #getting arguments user entered
-    args = parser.parse_args()
+    #args = parser.parse_args()
 
     #results = IPAddressCount(args.filename)
     results = IPAddressCount("m5-access.log")
 
     #get just the IP of the IP that is seen the most
-    highest_ip = results.split("\n")[-2].split()[1]
+    highest_IP = results.split("\n")[-2].split()[1]
 
-    print(highest_ip)
+    print(highest_IP)
 
-    highest_url = IPLookUp(highest_ip)
-    print(highest_url[:250])
+    #getting json info from api
+    IP_info = IPLookUp(highest_IP)
 
-    myHTML = bs4. BeautifulSoup(highest_url, features="html.parser")
-    print(myHTML.find_all("dd",class_ = "col-8 text-monospace")[1].text)
+    #turning the json file to a dic
+    IP_info = (json.loads(IP_info))
+
+    #printing the city and org
+    print(f"", IP_info["city"], "\n", IP_info["org"])
 
     apache_log_analysis = open("apache_analysis.txt", "w")
-
-    apache_log_analysis.write(highest_url)
-
+    apache_log_analysis.write(str(IP_info))
     apache_log_analysis.close
-
-    
             
 if __name__ == "__main__":
     main()
